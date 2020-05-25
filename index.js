6772ff8bc800c0714fe8b935001c621212e7c7a3
Cris Mihalache <me@f3rno.com>
2020-05-25 18:11:57 +0700

    (wip)

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 'use strict'
 
-const mapMethods = require('./lib/map_methods')
-const genericMethods = require('./lib/generic_methods')
-const collectionMethods = require('./lib/collection_methods')
+const HFDBTemplateAdapter = require('./lib/index')
 
 /**
- * DB init method; can take any arguments, but must return the example object
- * structure.
+ * This module serves as a template for creating new database adapters for the
+ * honey framework. All adapters must implement the methods broken out here,
+ * and provide a custom initialisation function. This system enables the use of
+ * the Honey Framework with multiple database backends. The only requirement
+ * for valid adapters is that all method signatures remain the same, and all
+ * return values are promises that resolve to the standardized values. All
+ * methods are asynchronous.
+
+ * ### Features
+ * * Provides a template enumerating all required methods for
+ *   {@link module:bfx-hf-models|bfx-hf-models} compatibility
+
+ * ### Installation
+
+ * ```bash
+ * npm i --save bfx-hf-models-adapter-template
+ * ```
+
+ * ### Quickstart & Example
+ * Although the resulting DB instance is useless as it does not have an actual
+ * DB backend to write/read data, the following example illustrates how to pass
+ * an adapter to a {@link module:bfx-hf-models|bfx-hf-models} instance:
+
+ * ```js
+ * const HFDBTemplateAdapter = require('bfx-hf-models-adapter-template')
+ * const { schema: HFDBBitfinexSchema } = require('bfx-hf-ext-plugin-bitfinex')
+ * const HFDB = require('bfx-hf-models')
+
+ * const db = new HFDB({
+ *   schema: HFDBBitfinexSchema,
+ *   adapter: HFDBTemplateAdapter()
+ * })
+
+ * // db is now a valid bfx-hf-models database instance
+ * ```
  *
- * @return {Object} hfDBAdapter
+ * @license Apache-2.0
+ * @module bfx-hf-models-adapter-template
  */
-module.exports = () => {
-  return {
-    db: {}, // DB object provided to all methods
-    name: 'template',
-    close: () => {}, // should close the DB connection
-    mapMethods,
-    collectionMethods,
-    genericMethods
-  }
-}
+
+module.exports = HFDBTemplateAdapter
